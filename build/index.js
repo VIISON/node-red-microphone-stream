02d0ae8794d8868e4ef7404cd35f7654370f28fb
Thomas Nordquist <thomas.nordquist@viison.com>
2018-12-21 15:51:38 +0100

    Increase loudness (noisereduciton & compression)

--- build/index.js
+++ build/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_sonos_voice_streaming_1 = require("node-sonos-voice-streaming");
+var child_process_1 = require("child_process");
+var fs_1 = require("fs");
 module.exports = function (RED) {
     function universalNode(config) {
         RED.nodes.createNode(this, config);
@@ -9,6 +11,14 @@ module.exports = function (RED) {
         console.log('connecting to' + config.url);
         client.on(node_sonos_voice_streaming_1.eventNames.audioLiveStream.ready, function () {
             console.log("ready");
+            var compandArgs = 'compand 0.3,1 6:-10 15';
+            if (fs_1.existsSync('/tmp/noise.prof')) {
+                console.log('hello');
+                client.record(("noisered /tmp/noise.prof 0.21 " + compandArgs).split(' '));
+            }
+            else {
+                client.record(compandArgs.split(' '));
+            }
             node.send({
                 topic: 'ready'
             });
@@ -17,6 +27,14 @@ module.exports = function (RED) {
             var value = parseInt(msg.payload, 10);
             if (value === 1) {
                 console.log("start");
+                var p_1 = child_process_1.spawn('rec', '/tmp/noise-audio.wav'.split(' '));
+                setTimeout(function () {
+                    p_1.kill("SIGTERM");
+                    p_1.once('exit', function () {
+                        console.log('create noise profile');
+                        child_process_1.spawn('sox', '/tmp/noise-audio.wav -n noiseprof /tmp/noise.prof'.split(' '));
+                    });
+                }, 500);
                 client.startStream();
             }
             else {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Red } from 'node-red'
 import { client as VoiceStreamingClient, eventNames } from 'node-sonos-voice-streaming'
+import { spawn } from 'child_process'
+import { existsSync } from 'fs'
 
 module.exports = function(RED) {
     function universalNode(config) {
@@ -10,6 +12,13 @@ module.exports = function(RED) {
         console.log('connecting to' + config.url)
         client.on(eventNames.audioLiveStream.ready, () => {
             console.log("ready")
+            let compandArgs = 'compand 0.3,1 6:-10 15'
+            if (existsSync('/tmp/noise.prof')) {
+                console.log('hello')
+                client.record(`noisered /tmp/noise.prof 0.21 ${compandArgs}`.split(' '))
+            } else {
+                client.record(compandArgs.split(' '))
+            }
 
             node.send({
                 topic: 'ready'
@@ -20,6 +29,14 @@ module.exports = function(RED) {
             const value = parseInt(msg.payload, 10)
             if (value === 1) {
                 console.log("start")
+                let p = spawn('rec', '/tmp/noise-audio.wav'.split(' '));
+                setTimeout(() => {
+                    p.kill("SIGTERM")
+                    p.once('exit', () => {
+                        console.log('create noise profile')
+                        spawn('sox', '/tmp/noise-audio.wav -n noiseprof /tmp/noise.prof'.split(' '))
+                    });
+                }, 500)
                 client.startStream()
             } else {
                 console.log("stop")
